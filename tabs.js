567c085aa6ed7f29ed5b94b004c648667f35c139
vladinquisidor <inge__garcia@hotmail.com>
2016-10-17 17:52:56 -0500

    Update tabs.js
    borrar basura

--- tabs.js
+++ tabs.js
@@ -2,16 +2,6 @@ $(document).ready(function () {
   // Escribe tu código aquí
 
   $('#tab1').show()
-
-  /*
-
-  $('.tabs > li:nth-child(1)').click(function(){ $('#tab2').hide(); $('#tab3').hide(); $('#tab1').show();  $('.tabs > li:nth-child(1)').addClass('active'); $('.tabs > li:nth-child(2)').removeClass('active'); $('.tabs > li:nth-child(3)').removeClass('active'); });
-
-  $('.tabs > li:nth-child(2)').click(function(){ $('#tab1').hide(); $('#tab3').hide(); $('#tab2').show();  $('.tabs > li:nth-child(2)').addClass('active'); $('.tabs > li:nth-child(1)').removeClass('active'); $('.tabs > li:nth-child(3)').removeClass('active'); });
-
-  $('.tabs > li:nth-child(3)').click(function(){ $('#tab2').hide(); $('#tab1').hide(); $('#tab3').show();  $('.tabs > li:nth-child(3)').addClass('active'); $('.tabs > li:nth-child(1)').removeClass('active'); $('.tabs > li:nth-child(2)').removeClass('active'); });
-  */
-
   $('.tabs > li').click(function(){ 
     console.log(this);
     $('.tabs > li').removeClass('active'); 
